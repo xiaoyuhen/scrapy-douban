57521618793b718b78b219eaf2d93ee7dc64c6fd
xiaoyuhen <xiaoyuhen1994@gmail.com>
2017-10-20 23:18:19 +0800

    add douban_poster

--- douban_poster/douban_poster/pipelines.py
+++ douban_poster/douban_poster/pipelines.py
@@ -5,7 +5,8 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-from scrapy.pipeline.images import ImagesPipeline
+
+from scrapy.pipelines.images import ImagesPipeline
 from scrapy.exceptions import DropItem
 from scrapy.http import Request
 
---
+++ douban_poster/douban_poster/spiders/poster.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import re
+from scrapy.spiders import Spider
+from scrapy import Request
+
+from douban_poster.items import DoubanPosterItem
+
+
+class DoubanPosterSpider(Spider):
+  name = 'douban_poster'
+
+  def start_requests(self):
+    url = 'https://movie.douban.com/subject/1292052/photos?start=0'
+    yield Request(url)
+
+
+  def parse(self, response):
+    sites = response.xpath('//div[@id="wrapper"]//div[@class="article"]/ul/li')
+    if sites:
+      for site in sites:
+
+        item = DoubanPosterItem()
+        item_src = site.xpath('./div[@class="cover"]/a/img/@src')
+        item['image_urls'] = item_src.extract()
+        yield item
+
+      page_num = re.search(r'start=(\d+)', response.url).group(1)
+      page_num = 'start=' + str(int(page_num) + 30)
+      next_url = re.sub(r'start=\d+', page_num, response.url)
+      yield Request(next_url)
+
